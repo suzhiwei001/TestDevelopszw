a46bd5680092af77940e2ac85ff2a6fe88a6a7b7
su_zhiwei <su_zhiwei@hoperun.com>
2018-03-30 15:20:43 +0800

    去除不需要的代码

--- suzhiwei-test/src/com/szw/test/sss.java
+++ suzhiwei-test/src/com/szw/test/sss.java
@@ -4,35 +4,24 @@
 package com.szw.test;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import com.alibaba.fastjson.JSONObject;
-import com.szw.pojo.BussElement;
-import com.szw.service.BussElementService;
 
 /**
  * @author suzhiwei
- *
  */
 public class sss {
 public static void main(String[] args) {
-	BussElementService bussElementService  =  new BussElementService();
-	List<BussElement> findByBussElement = bussElementService.findByBussElement();
 	Map<String,Object> hm1 = new HashMap<>();
-	
 	Map<String,String> hm = new HashMap<>();
 	hm.put("ww", "dd");
 	hm.put("ddd", "www");
-	
-	
-	
 	Map<String,String> hm2 = new HashMap<>();
 	hm2.put("ww", "dd");
 	hm2.put("ddd", "www");
 	hm1.put("ss", hm);
 	hm1.put("ss2", hm2);
 	String parameter = JSONObject.toJSONString(hm1);
-	System.out.println(parameter);
 	System.out.println(parameter);
 }
 }
